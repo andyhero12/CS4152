c2a8bfcd2e358ec37926643108d819d24193a1bb
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-03-21 21:03:46 -0400

    add map representation to overworld

--- source/OverWorld.hpp
+++ source/OverWorld.hpp
@@ -24,15 +24,17 @@
 
 class OverWorld{
 private:
-        std::shared_ptr<Dog> _dog;
-        std::shared_ptr<Devil> _devil;
-        std::vector<std::shared_ptr<Decoy>> _decoys;
-        std::vector<std::shared_ptr<Spawner>> _spawners;
-        std::shared_ptr<BaseSet> _bases;
-        std::shared_ptr<cugl::JsonValue> _constants;
-        std::shared_ptr<cugl::AssetManager> _assets;
-        cugl::Size _totalSize;
-        AttackPolygons _attackPolygonSet;
+    std::shared_ptr<Dog> _dog;
+    std::shared_ptr<Devil> _devil;
+    std::vector<std::shared_ptr<Decoy>> _decoys;
+    std::vector<std::shared_ptr<Spawner>> _spawners;
+    std::shared_ptr<BaseSet> _bases;
+    std::shared_ptr<cugl::JsonValue> _constants;
+    std::shared_ptr<cugl::AssetManager> _assets;
+    cugl::Size _totalSize;
+    AttackPolygons _attackPolygonSet;
+    
+        
 public:
     
     OverWorld(){
@@ -41,6 +43,8 @@ public:
     ~OverWorld(){
         
     }
+    
+    
     
     void reset(cugl::Size resetSize);
     bool init(const std::shared_ptr<cugl::AssetManager>& assets, cugl::Size totalSize);
@@ -55,7 +59,21 @@ public:
     
     void draw(const std::shared_ptr<cugl::SpriteBatch>& batch,cugl::Size totalSize);
     
+    // Might want to be more specific with tile types
+    enum Terrain {
+        PASSABLE,
+        IMPASSIBLE
+    };
     
+    // Information for a tile, add to this later since idk what this will include
+    class TileInfo {
+        public:
+        private:
+            Terrain type;
+    };
+    
+    // Matrix with information about the overworld
+    std::vector<std::vector<TileInfo>> overworld;
     
     std::shared_ptr<Dog> getDog(){
         return _dog;
@@ -78,4 +96,26 @@ public:
     void setDevil(std::shared_ptr<Devil> m_devil){
         _devil = m_devil;
     }
+    
+    /**
+    Get the type of terrain at the tile at (x, y)
+     */
+    Terrain getTerrain(int x, int y);
+    
+    /**
+    Whether the terrain at (x, y) is passable
+     */
+    bool isPassable(int x, int y);
+    
+    /**
+     Gets the width of the overworld in tile coordinates
+     */
+    int getRows();
+    
+    /**
+     Gets the height of the overworld in tile coordinates
+     */
+    int getCols();
+    
+    
 };
